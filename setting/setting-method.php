feb0348338ea8d94690afaef3f213c4041af377f
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-03-22 16:10:23 +0900

    fin setting-method

--- setting/setting-method.php
+++ setting/setting-method.php
@@ -12,11 +12,14 @@
     <h1>님은 무엇이 고민인가요?</h1> <br>
     <p> 개선하고 싶은 문제를 골라보세요. </p> <br>
     <div class="wrap">
-        <button class="select-method"> 깊은수면</button><br>
-        <button class="select-method"> 감정 다스리기</button><br>
-        <button class="select-method"> 집중과 몰입</button><br>
-        <button class="select-method"> 일상에서 벗어나기</button><br>
-        <button class="select-method"> 삶의 의미 찾기</button>
+        <div id="holder">
+            <a href="#" class="btn"><span class="icon icon-search"></span></a>
+                <button class="select-method"> 깊은수면</button><br>
+            <button class="select-method"> 감정 다스리기</button><br>
+            <button class="select-method"> 집중과 몰입</button><br>
+            <button class="select-method"> 일상에서 벗어나기</button><br>
+            <button class="select-method"> 삶의 의미 찾기</button>
+        </div>
     </div>
     <button class="button"> 선택</button>
 </body>
