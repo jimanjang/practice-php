4528f35725a2ac7e040ad9ba13e638c31bb70dda
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-03-22 15:55:50 +0900

    modify select-method

--- setting/setting-method.php
+++ setting/setting-method.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="setting-name.css?after">
+    <link rel="stylesheet" href="setting-method.css?after">
     <title>Document</title>
 </head>
 <body>
-    <h1><?php include 'setting-name.php';?></h1>
-    <?php
-        // 'user' 라는 폼 매개변수로 제출된 값 출력
-        print $_POST['user'];
-    ?>
+    <h1><?php include 'setting-name.php';?> </h1> <br>
     <h1>님은 무엇이 고민인가요?</h1> <br>
-    <p> 개선하고 싶은 문제를 골라보세요. </p>
-
+    <p> 개선하고 싶은 문제를 골라보세요. </p> <br>
+    <div class="wrap">
+        <button class="select-method"> 깊은수면</button><br>
+        <button class="select-method"> 감정 다스리기</button><br>
+        <button class="select-method"> 집중과 몰입</button><br>
+        <button class="select-method"> 일상에서 벗어나기</button><br>
+        <button class="select-method"> 삶의 의미 찾기</button>
+    </div>
+    <button class="button"> 선택</button>
 </body>
 </html>
